abd25218a7cb6423ec281f7328207eeb558882c9
SNQ-2001 <taishin05109@gmail.com>
2023-03-29 02:11:47 +0900

    Reduce ChatGPTModel

--- Core/Sources/Preferences/ChatGPTModel.swift
+++ Core/Sources/Preferences/ChatGPTModel.swift
@@ -4,14 +4,6 @@ public enum ChatGPTModel: String {
     case gpt35Turbo = "gpt-3.5-turbo"
 
     case gpt35Turbo0301 = "gpt-3.5-turbo-0301"
-
-    case textDavinci003 = "text-davinci-003"
-
-    case textCurie001 = "text-curie-001"
-
-    case textBabbage001 = "text-babbage-001"
-
-    case textAda001 = "text-ada-001"
 }
 
 public extension ChatGPTModel {
@@ -21,30 +13,14 @@ public extension ChatGPTModel {
             return "https://api.openai.com/v1/chat/completions"
         case .gpt35Turbo0301:
             return "https://api.openai.com/v1/chat/completions"
-        case .textDavinci003:
-            return "https://api.openai.com/v1/completions"
-        case .textCurie001:
-            return "https://api.openai.com/v1/completions"
-        case .textBabbage001:
-            return "https://api.openai.com/v1/completions"
-        case .textAda001:
-            return "https://api.openai.com/v1/completions"
         }
     }
 
     var maxToken: Int {
         switch self {
         case .gpt35Turbo:
-            return 4096
+            return 2049
         case .gpt35Turbo0301:
-            return 4096
-        case .textDavinci003:
-            return 4097
-        case .textCurie001:
-            return 2049
-        case .textBabbage001:
-            return 2049
-        case .textAda001:
             return 2049
         }
     }
