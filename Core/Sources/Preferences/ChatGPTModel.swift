8296726406fc021302b7b740fd5df071bcf9dd68
SNQ-2001 <taishin05109@gmail.com>
2023-03-29 00:49:37 +0900

    Add endpoint and maxToken to ChatGPTModel

--- Core/Sources/Preferences/ChatGPTModel.swift
+++ Core/Sources/Preferences/ChatGPTModel.swift
@@ -1,6 +1,10 @@
 import Foundation
 
 public enum ChatGPTModel: String {
+    case gpt35Turbo = "gpt-3.5-turbo"
+
+    case gpt35Turbo0301 = "gpt-3.5-turbo-0301"
+
     case textDavinci003 = "text-davinci-003"
 
     case textCurie001 = "text-curie-001"
@@ -8,16 +12,42 @@ public enum ChatGPTModel: String {
     case textBabbage001 = "text-babbage-001"
 
     case textAda001 = "text-ada-001"
+}
 
-    case codeDavinci002 = "code-davinci-002"
+extension ChatGPTModel {
+    var endpoint: String {
+        switch self {
+        case .gpt35Turbo:
+            return "https://api.openai.com/v1/chat/completions"
+        case .gpt35Turbo0301:
+            return "https://api.openai.com/v1/chat/completions"
+        case .textDavinci003:
+            return "https://api.openai.com/v1/completions"
+        case .textCurie001:
+            return "https://api.openai.com/v1/completions"
+        case .textBabbage001:
+            return "https://api.openai.com/v1/completions"
+        case .textAda001:
+            return "https://api.openai.com/v1/completions"
+        }
+    }
 
-    case codeCushman001 = "code-cushman-001"
-
-    case textDavinciEdit001 = "text-davinci-edit-001"
-
-    case gpt35Turbo = "gpt-3.5-turbo"
-
-    case gpt35Turbo0301 = "gpt-3.5-turbo-0301"
+    var maxToken: Int {
+        switch self {
+        case .gpt35Turbo:
+            return 4096
+        case .gpt35Turbo0301:
+            return 4096
+        case .textDavinci003:
+            return 4097
+        case .textCurie001:
+            return 2049
+        case .textBabbage001:
+            return 2049
+        case .textAda001:
+            return 2049
+        }
+    }
 }
 
 extension ChatGPTModel: CaseIterable {}
