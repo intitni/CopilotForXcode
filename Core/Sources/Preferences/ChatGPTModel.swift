db836378345c2e4e661e29e2c5e9a9314fa6169b
SNQ-2001 <taishin05109@gmail.com>
2023-03-31 01:29:25 +0900

    Add Model

--- Core/Sources/Preferences/ChatGPTModel.swift
+++ Core/Sources/Preferences/ChatGPTModel.swift
@@ -1,6 +1,14 @@
 import Foundation
 
 public enum ChatGPTModel: String {
+    case gpt4 = "gpt-4"
+
+    case gpt40314 = "gpt-4-0314"
+
+    case gpt432k = "gpt-4-32k"
+
+    case gpt432k0314 = "gpt-4-32k-0314"
+
     case gpt35Turbo = "gpt-3.5-turbo"
 
     case gpt35Turbo0301 = "gpt-3.5-turbo-0301"
@@ -12,7 +20,20 @@ public extension ChatGPTModel {
     }
 
     var maxToken: Int {
-        4096
+        switch self {
+        case .gpt4:
+            return 8192
+        case .gpt40314:
+            return 8192
+        case .gpt432k:
+            return 32768
+        case .gpt432k0314:
+            return 32768
+        case .gpt35Turbo:
+            return 4096
+        case .gpt35Turbo0301:
+            return 4096
+        }
     }
 }
 
