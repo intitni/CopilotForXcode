30fb56c6fdc34e8d6a9c04845e685e6839e1f72b
Shx Guo <int123c@gmail.com>
2023-04-14 19:19:53 +0800

    Add the boost QoS hack that doesn't really make any change
    https://developer.apple.com/forums/thread/682769

--- Core/Sources/XPCShared/XPCServiceProtocol.swift
+++ Core/Sources/XPCShared/XPCServiceProtocol.swift
@@ -3,6 +3,7 @@ import Foundation
 
 @objc(XPCServiceProtocol)
 public protocol XPCServiceProtocol {
+    func boostQoS(withReply reply: @escaping () -> Void)
     func checkStatus(withReply reply: @escaping (String?, Error?) -> Void)
     func signInInitiate(withReply reply: @escaping (String?, String?, Error?) -> Void)
     func signInConfirm(
