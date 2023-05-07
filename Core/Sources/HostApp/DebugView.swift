9ca765e4023481d7b13ce2239a628845e9190aa2
Shx Guo <int123c@gmail.com>
2023-05-07 23:35:43 +0800

    Migrate debug settings to HostApp

---
+++ Core/Sources/HostApp/DebugView.swift
@@ -0,0 +1,40 @@
+import Preferences
+import SwiftUI
+
+final class DebugSettings: ObservableObject {
+    @AppStorage(\.disableLazyVStack) var disableLazyVStack
+    @AppStorage(\.preCacheOnFileOpen) var preCacheOnFileOpen
+    @AppStorage(\.useCustomScrollViewWorkaround) var useCustomScrollViewWorkaround
+    @AppStorage(\.triggerActionWithAccessibilityAPI) var triggerActionWithAccessibilityAPI
+    init() {}
+}
+
+struct DebugSettingsView: View {
+    @StateObject var settings = DebugSettings()
+
+    var body: some View {
+        Section {
+            Form {
+                Toggle(isOn: $settings.disableLazyVStack) {
+                    Text("Disable LazyVStack")
+                }
+                Toggle(isOn: $settings.preCacheOnFileOpen) {
+                    Text("Cache editor information on file open")
+                }
+                Toggle(isOn: $settings.useCustomScrollViewWorkaround) {
+                    Text("Use custom scroll view workaround for smooth scrolling")
+                }
+                Toggle(isOn: $settings.triggerActionWithAccessibilityAPI) {
+                    Text("Trigger action with AccessibilityAPI")
+                }
+            }
+        }
+    }
+}
+
+struct DebugSettingsView_Preview: PreviewProvider {
+    static var previews: some View {
+        DebugSettingsView()
+    }
+}
+
