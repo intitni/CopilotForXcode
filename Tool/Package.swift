1cd60d1d2cc705bda1dc71ea65a6352208abf9b8
Shx Guo <int123c@gmail.com>
2023-05-22 15:32:58 +0800

    Add target LangChainService

--- Tool/Package.swift
+++ Tool/Package.swift
@@ -6,23 +6,25 @@ import PackageDescription
 let package = Package(
     name: "Tool",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Tool",
-            targets: ["Tool"]),
+            targets: ["LangChainService"]
+        ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/pvieito/PythonKit.git", from: "0.3.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Tool",
-            dependencies: []),
+            name: "LangChainService",
+            dependencies: [
+                "PythonKit",
+            ]
+        ),
         .testTarget(
-            name: "ToolTests",
-            dependencies: ["Tool"]),
+            name: "LangChainServiceTests",
+            dependencies: ["LangChainService"]
+        ),
     ]
 )
+
