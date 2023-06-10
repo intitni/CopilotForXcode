5f25b7cf59589a968e71a5f94bf1dc9eea2dfd19
Shx Guo <int123c@gmail.com>
2023-06-10 16:04:51 +0800

    Add ShortcutChatPlugin

--- Core/Sources/ChatService/AllPlugins.swift
+++ Core/Sources/ChatService/AllPlugins.swift
@@ -1,10 +1,13 @@
 import ChatPlugin
 import MathChatPlugin
 import SearchChatPlugin
+import ShortcutChatPlugin
 
 let allPlugins: [ChatPlugin.Type] = [
     TerminalChatPlugin.self,
     AITerminalChatPlugin.self,
     MathChatPlugin.self,
     SearchChatPlugin.self,
+    ShortcutChatPlugin.self,
 ]
+
