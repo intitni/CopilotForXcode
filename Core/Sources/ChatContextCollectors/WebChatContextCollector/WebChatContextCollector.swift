60601045d6bf102728a9a01372006eb0c009e81a
Shx Guo <int123c@gmail.com>
2023-06-28 16:28:25 +0800

    Update WebChatContextCollector to detect links from memory

--- Core/Sources/ChatContextCollectors/WebChatContextCollector/WebChatContextCollector.swift
+++ Core/Sources/ChatContextCollectors/WebChatContextCollector/WebChatContextCollector.swift
@@ -1,11 +1,10 @@
 import ChatContextCollector
 import Foundation
 import OpenAIService
-import SuggestionModel
 
 public final class WebChatContextCollector: ChatContextCollector {
     var recentLinks = [String]()
-    
+
     public init() {}
 
     public func generateContext(
@@ -28,8 +27,24 @@ public final class WebChatContextCollector: ChatContextCollector {
 }
 
 extension WebChatContextCollector {
-    static func detectLinks(from: [ChatMessage]) -> [String] {
-        return []
+    static func detectLinks(from messages: [ChatMessage]) -> [String] {
+        var links = [String]()
+        let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+        for message in messages {
+            guard let content = message.content else { continue }
+            let matches = detector?.matches(
+                in: content,
+                options: [],
+                range: NSRange(content.startIndex..., in: content)
+            )
+
+            for match in matches ?? [] {
+                guard let range = Range(match.range, in: content) else { continue }
+                let url = content[range]
+                links.append(String(url))
+            }
+        }
+        return links
     }
 }
 
