ed252e64832b209f5c7cf25f06e931b3082ebb7d
Shx Guo <int123c@gmail.com>
2023-07-05 23:36:46 +0800

    Update ChatTab to better handle tab titles

---
+++ Core/Sources/ChatTab/ChatGPT/ChatGPTChatTab.swift
@@ -0,0 +1,113 @@
+import ChatService
+import Combine
+import Foundation
+import SwiftUI
+
+/// A chat tab that provides a context aware chat bot, powered by ChatGPT.
+public class ChatGPTChatTab: ChatTab {
+    public let service: ChatService
+    public let provider: ChatProvider
+    private var cancellable = Set<AnyCancellable>()
+
+    public func buildView() -> any View {
+        ChatPanel(chat: provider)
+    }
+
+    public init(service: ChatService = .init()) {
+        self.service = service
+        provider = .init(service: service)
+        super.init(id: "Chat-" + provider.id.uuidString, title: "Chat")
+        
+        provider.$history.sink { [weak self] _ in
+            if let title = self?.provider.title {
+                self?.title = title
+            }
+        }.store(in: &cancellable)
+    }
+}
+
+extension ChatProvider {
+    convenience init(service: ChatService) {
+        self.init(pluginIdentifiers: service.allPluginCommands)
+
+        let cancellable = service.objectWillChange.sink { [weak self] in
+            guard let self else { return }
+            Task { @MainActor in
+                self.history = (await service.memory.history).map { message in
+                    .init(
+                        id: message.id,
+                        role: {
+                            switch message.role {
+                            case .system: return .ignored
+                            case .user: return .user
+                            case .assistant:
+                                if let text = message.summary ?? message.content, !text.isEmpty {
+                                    return .assistant
+                                }
+                                return .ignored
+                            case .function: return .function
+                            }
+                        }(),
+                        text: message.summary ?? message.content ?? ""
+                    )
+                }
+                self.isReceivingMessage = service.isReceivingMessage
+                self.systemPrompt = service.systemPrompt
+                self.extraSystemPrompt = service.extraSystemPrompt
+            }
+        }
+
+        service.objectWillChange.send()
+
+        onMessageSend = { [cancellable] message in
+            _ = cancellable
+            Task {
+                try await service.send(content: message)
+            }
+        }
+        onStop = {
+            Task {
+                await service.stopReceivingMessage()
+            }
+        }
+
+        onClear = {
+            Task {
+                await service.clearHistory()
+            }
+        }
+
+        onDeleteMessage = { id in
+            Task {
+                await service.deleteMessage(id: id)
+            }
+        }
+
+        onResendMessage = { id in
+            Task {
+                try await service.resendMessage(id: id)
+            }
+        }
+
+        onResetPrompt = {
+            Task {
+                await service.resetPrompt()
+            }
+        }
+
+        onRunCustomCommand = { command in
+            Task {
+                #warning("how to handle custom command?")
+//                let commandHandler = PseudoCommandHandler()
+//                await commandHandler.handleCustomCommand(command)
+            }
+        }
+
+        onSetAsExtraPrompt = { id in
+            Task {
+                await service.setMessageAsExtraPrompt(id: id)
+            }
+        }
+    }
+}
+
--- Core/Sources/ChatTab/ChatTab.swift
+++ Core/Sources/ChatTab/ChatTab.swift
@@ -1,24 +1,76 @@
+import ComposableArchitecture
 import Foundation
 import SwiftUI
 
-open class BaseChatTab: Equatable {
-    public let id: UUID
-    
-    public static func == (lhs: BaseChatTab, rhs: BaseChatTab) -> Bool {
-        lhs.id == rhs.id
-    }
-    
-    init(id: UUID) {
+public struct ChatTabInfo: Identifiable, Equatable {
+    public var id: String
+    public var title: String
+
+    public init(id: String, title: String) {
         self.id = id
+        self.title = title
     }
-    
+}
+
+public struct ChatTabInfoPreferenceKey: PreferenceKey {
+    public static var defaultValue: [ChatTabInfo] = []
+    public static func reduce(value: inout [ChatTabInfo], nextValue: () -> [ChatTabInfo]) {
+        value.append(contentsOf: nextValue())
+    }
+}
+
+/// Every chat tab should conform to this type.
+public typealias ChatTab = BaseChatTab & ChatTabType
+
+open class BaseChatTab: Equatable {
+    final class InfoObservable: ObservableObject {
+        @Published var id: String
+        @Published var title: String
+        init(id: String, title: String) {
+            self.title = title
+            self.id = id
+        }
+    }
+
+    struct ContentView: View {
+        @ObservedObject var info: InfoObservable
+        var buildView: () -> any View
+        var body: some View {
+            AnyView(buildView())
+                .preference(
+                    key: ChatTabInfoPreferenceKey.self,
+                    value: [ChatTabInfo(
+                        id: info.id,
+                        title: info.title
+                    )]
+                )
+        }
+    }
+
+    public let id: String
+    public var title: String {
+        didSet { info.title = title }
+    }
+
+    let info: InfoObservable
+
+    public init(id: String, title: String) {
+        self.id = id
+        self.title = title
+        info = InfoObservable(id: id, title: title)
+    }
+
     @ViewBuilder
     public var body: some View {
         if let tab = self as? ChatTabType {
-            AnyView(tab.buildView()).id(id)
+            ContentView(info: info, buildView: tab.buildView).id(info.id)
         } else {
-            EmptyView()
+            EmptyView().id(info.id)
         }
+    }
+
+    public static func == (lhs: BaseChatTab, rhs: BaseChatTab) -> Bool {
+        lhs.id == rhs.id
     }
 }
 
@@ -27,24 +79,16 @@ public protocol ChatTabType {
     func buildView() -> any View
 }
 
-public typealias ChatTab = BaseChatTab & ChatTabType
-
-public class ChatGPTChatTab: ChatTab {
-    public var provider: ChatProvider
-    
-    public func buildView() -> any View {
-        ChatPanel(chat: provider)
-    }
-
-    public init(provider: ChatProvider) {
-        self.provider = provider
-        super.init(id: provider.id)
-    }
-}
-
 public class EmptyChatTab: ChatTab {
     public func buildView() -> any View {
-        EmptyView()
+        VStack {
+            Text("Empty-\(id)")
+        }
+        .background(Color.blue)
+    }
+
+    public init(id: String = UUID().uuidString) {
+        super.init(id: id, title: "Empty")
     }
 }
 
