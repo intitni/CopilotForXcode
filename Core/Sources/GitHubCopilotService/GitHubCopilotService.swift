7c3e584956a281e1887c384b94f38704fc68b905
Shx Guo <int123c@gmail.com>
2023-07-10 14:14:40 +0800

    Bump GitHub Copilot to 1.9.1

--- Core/Sources/GitHubCopilotService/GitHubCopilotService.swift
+++ Core/Sources/GitHubCopilotService/GitHubCopilotService.swift
@@ -170,8 +170,6 @@ public class GitHubCopilotBaseService {
             try await server.sendRequest(GitHubCopilotRequest.SetEditorInfo())
         }
     }
-    
-    
 
     public static func createFoldersIfNeeded() throws -> (
         applicationSupportURL: URL,
