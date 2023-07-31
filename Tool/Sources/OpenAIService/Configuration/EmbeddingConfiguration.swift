0a59dcdccd3521c90ed8f03303a5e85f06c4f2e9
Shx Guo <int123c@gmail.com>
2023-08-01 00:26:48 +0800

    Update Azure OpenAI api version

--- Tool/Sources/OpenAIService/Configuration/EmbeddingConfiguration.swift
+++ Tool/Sources/OpenAIService/Configuration/EmbeddingConfiguration.swift
@@ -19,7 +19,7 @@ public extension EmbeddingConfiguration {
         case .azureOpenAI:
             let baseURL = UserDefaults.shared.value(for: \.azureOpenAIBaseURL)
             let deployment = UserDefaults.shared.value(for: \.azureChatGPTDeployment)
-            let version = "2023-05-15"
+            let version = "2023-07-01-preview"
             if baseURL.isEmpty { return "" }
             return "\(baseURL)/openai/deployments/\(deployment)/embeddings?api-version=\(version)"
         }
