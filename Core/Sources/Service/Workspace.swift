a211955b588dc1cdd7d285d749f10348ef50721e
Shx Guo <int123c@gmail.com>
2023-08-01 17:24:11 +0800

    Ignore mlmodel files when creating Filespace

--- Core/Sources/Service/Workspace.swift
+++ Core/Sources/Service/Workspace.swift
@@ -129,6 +129,13 @@ final class Workspace {
             "Suggestion feature is disabled for this project."
         }
     }
+    
+    struct UnsupportedFileError: Error, LocalizedError {
+        var extensionName: String
+        var errorDescription: String? {
+            "File type \(extensionName) unsupported."
+        }
+    }
 
     let projectRootURL: URL
     let openedFileRecoverableStorage: OpenedFileRecoverableStorage
@@ -223,6 +230,11 @@ final class Workspace {
     static func fetchOrCreateWorkspaceIfNeeded(fileURL: URL) async throws
         -> (workspace: Workspace, filespace: Filespace)
     {
+        let ignoreFileExtensions = ["mlmodel"]
+        if ignoreFileExtensions.contains(fileURL.pathExtension) {
+            throw UnsupportedFileError(extensionName: fileURL.pathExtension)
+        }
+        
         // If we know which project is opened.
         if let currentProjectURL = try await Environment.fetchCurrentProjectRootURLFromXcode() {
             if let existed = workspaces[currentProjectURL] {
