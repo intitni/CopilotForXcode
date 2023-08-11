f6177b72ebf65aab2ff9581a9c8f51b139d4c042
Shx Guo <int123c@gmail.com>
2023-08-11 17:01:49 +0800

    Bump highlight.js version

--- Tool/Package.swift
+++ Tool/Package.swift
@@ -39,7 +39,7 @@ let package = Package(
         .package(url: "https://github.com/ChimeHQ/JSONRPC", exact: "0.6.0"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
         .package(url: "https://github.com/unum-cloud/usearch", from: "0.19.1"),
-        .package(url: "https://github.com/raspu/Highlightr", from: "2.1.0"),
+        .package(url: "https://github.com/intitni/Highlightr", branch: "bump-highlight-js-version"),
         .package(url: "https://github.com/JohnSundell/Splash", branch: "master"),
         .package(
             url: "https://github.com/pointfreeco/swift-composable-architecture",
