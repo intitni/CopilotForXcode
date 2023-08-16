29afe7ef75b7c4a362d2f099aa6ee10203a97cc5
Shx Guo <int123c@gmail.com>
2023-08-16 21:06:40 +0800

    Update tests

--- Core/Tests/ServiceTests/Environment.swift
+++ Core/Tests/ServiceTests/Environment.swift
@@ -1,31 +1,14 @@
 import AppKit
 import Client
-import SuggestionModel
-import GitHubCopilotService
 import Environment
 import Foundation
+import GitHubCopilotService
+import SuggestionModel
+import Workspace
 import XCTest
 import XPCShared
 
 @testable import Service
-
-@ServiceActor func clearEnvironment() {
-    workspaces = [:]
-
-    Environment.now = { Date() }
-
-    Environment.fetchCurrentProjectRootURLFromXcode = { 
-        URL(fileURLWithPath: "/path/to/project")
-    }
-
-    Environment.fetchCurrentFileURL = {
-        URL(fileURLWithPath: "/path/to/project/file.swift")
-    }
-
-    Environment.triggerAction = { _ in }
-    
-    Environment.guessProjectRootURLForFile = { $0 }
-}
 
 func completion(text: String, range: CursorRange, uuid: String = "") -> CodeSuggestion {
     .init(text: text, position: range.start, uuid: uuid, range: range, displayText: text)
@@ -35,27 +18,27 @@ class MockSuggestionService: GitHubCopilotSuggestionServiceType {
     func terminate() async {
         fatalError()
     }
-    
+
     func cancelRequest() async {
         fatalError()
     }
-    
+
     func notifyOpenTextDocument(fileURL: URL, content: String) async throws {
         fatalError()
     }
-    
+
     func notifyChangeTextDocument(fileURL: URL, content: String) async throws {
         fatalError()
     }
-    
+
     func notifyCloseTextDocument(fileURL: URL) async throws {
         fatalError()
     }
-    
+
     func notifySaveTextDocument(fileURL: URL) async throws {
         fatalError()
     }
-    
+
     var completions = [CodeSuggestion]()
     var accepted: String?
     var rejected: [String] = []
@@ -85,3 +68,4 @@ class MockSuggestionService: GitHubCopilotSuggestionServiceType {
         rejected = completions.map(\.uuid)
     }
 }
+
