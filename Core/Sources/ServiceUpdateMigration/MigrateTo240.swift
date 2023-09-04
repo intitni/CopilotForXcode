7cb8e6e2d39bce94dd07b8ae5c7c43e5bf27b86c
Shx Guo <int123c@gmail.com>
2023-09-04 10:59:53 +0800

    Fix migrateTo240 to apply even if the settings already has a default value

--- Core/Sources/ServiceUpdateMigration/MigrateTo240.swift
+++ Core/Sources/ServiceUpdateMigration/MigrateTo240.swift
@@ -10,102 +10,107 @@ func migrateTo240(
     let finishedMigrationKey = "MigrateTo240Finished"
     if defaults.bool(forKey: finishedMigrationKey) { return }
 
-    let chatModelOpenAIId = UUID().uuidString
-    let chatModelAzureOpenAIId = UUID().uuidString
-    let embeddingModelOpenAIId = UUID().uuidString
-    let embeddingModelAzureOpenAIId = UUID().uuidString
-
-    let openAIAPIKeyName = "OpenAI"
-    let openAIAPIKey = defaults.deprecatedValue(for: \.openAIAPIKey)
-    if !openAIAPIKey.isEmpty {
-        try keychain.update(openAIAPIKey, key: openAIAPIKeyName)
-    }
-
-    let azureOpenAIAPIKeyName = "Azure OpenAI"
-    let azureOpenAIAPIKey = defaults.deprecatedValue(for: \.azureOpenAIAPIKey)
-    if !azureOpenAIAPIKey.isEmpty {
-        try keychain.update(azureOpenAIAPIKey, key: azureOpenAIAPIKeyName)
-    }
-
-    defaults.setupDefaultValue(for: \.chatModels, defaultValue: {
-        let openAIModel = ChatGPTModel(rawValue: defaults.deprecatedValue(for: \.chatGPTModel))
-
-        let openAI = ChatModel(
-            id: chatModelOpenAIId,
-            name: "OpenAI",
-            format: .openAI,
-            info: .init(
-                apiKeyName: openAIAPIKeyName,
-                baseURL: defaults.deprecatedValue(for: \.openAIBaseURL),
-                maxTokens: openAIModel?.maxToken ?? defaults
-                    .deprecatedValue(for: \.chatGPTMaxToken),
-                modelName: openAIModel?.rawValue ?? defaults
-                    .deprecatedValue(for: \.chatGPTModel)
-            )
-        )
-        let azureOpenAI = ChatModel(
-            id: chatModelAzureOpenAIId,
-            name: "Azure OpenAI",
-            format: .azureOpenAI,
-            info: .init(
-                apiKeyName: azureOpenAIAPIKeyName,
-                baseURL: defaults.deprecatedValue(for: \.azureOpenAIBaseURL),
-                maxTokens: defaults.deprecatedValue(for: \.chatGPTMaxToken),
-                modelName: defaults
-                    .deprecatedValue(for: \.azureChatGPTDeployment)
-            )
-        )
-
-        return [openAI, azureOpenAI]
-    }())
-
-    defaults.setupDefaultValue(for: \.defaultChatFeatureChatModelId, defaultValue: {
-        if defaults.deprecatedValue(for: \.chatFeatureProvider) == .azureOpenAI {
-            return chatModelAzureOpenAIId
+    do {
+        let chatModelOpenAIId = UUID().uuidString
+        let chatModelAzureOpenAIId = UUID().uuidString
+        let embeddingModelOpenAIId = UUID().uuidString
+        let embeddingModelAzureOpenAIId = UUID().uuidString
+        
+        let openAIAPIKeyName = "OpenAI"
+        let openAIAPIKey = defaults.deprecatedValue(for: \.openAIAPIKey)
+        if !openAIAPIKey.isEmpty {
+            try keychain.update(openAIAPIKey, key: openAIAPIKeyName)
         }
-        return chatModelOpenAIId
-    }())
-
-    defaults.setupDefaultValue(for: \.embeddingModels, defaultValue: {
-        let openAIModel = OpenAIEmbeddingModel(
-            rawValue: defaults.deprecatedValue(for: \.embeddingModel)
-        )
-
-        let openAI = EmbeddingModel(
-            id: embeddingModelOpenAIId,
-            name: "OpenAI",
-            format: .openAI,
-            info: .init(
-                apiKeyName: openAIAPIKeyName,
-                baseURL: defaults.deprecatedValue(for: \.openAIBaseURL),
-                maxTokens: openAIModel?.maxToken ?? 8191,
-                modelName: openAIModel?.rawValue ?? defaults.deprecatedValue(for: \.embeddingModel)
-            )
-        )
-
-        let azureOpenAI = EmbeddingModel(
-            id: embeddingModelAzureOpenAIId,
-            name: "Azure OpenAI",
-            format: .azureOpenAI,
-            info: .init(
-                apiKeyName: azureOpenAIAPIKeyName,
-                baseURL: defaults.deprecatedValue(for: \.azureOpenAIBaseURL),
-                maxTokens: 8191,
-                modelName: defaults
-                    .deprecatedValue(for: \.azureEmbeddingDeployment)
-            )
-        )
-
-        return [openAI, azureOpenAI]
-    }())
-
-    defaults.setupDefaultValue(for: \.defaultChatFeatureEmbeddingModelId, defaultValue: {
-        if defaults.deprecatedValue(for: \.embeddingFeatureProvider) == .azureOpenAI {
-            return embeddingModelAzureOpenAIId
+        
+        let azureOpenAIAPIKeyName = "Azure OpenAI"
+        let azureOpenAIAPIKey = defaults.deprecatedValue(for: \.azureOpenAIAPIKey)
+        if !azureOpenAIAPIKey.isEmpty {
+            try keychain.update(azureOpenAIAPIKey, key: azureOpenAIAPIKeyName)
         }
-        return embeddingModelOpenAIId
-    }())
-    
-    defaults.set(true, forKey: finishedMigrationKey)
+        
+        defaults.set({
+            let openAIModel = ChatGPTModel(rawValue: defaults.deprecatedValue(for: \.chatGPTModel))
+            
+            let openAI = ChatModel(
+                id: chatModelOpenAIId,
+                name: "OpenAI",
+                format: .openAI,
+                info: .init(
+                    apiKeyName: openAIAPIKeyName,
+                    baseURL: defaults.deprecatedValue(for: \.openAIBaseURL),
+                    maxTokens: openAIModel?.maxToken ?? defaults
+                        .deprecatedValue(for: \.chatGPTMaxToken),
+                    modelName: openAIModel?.rawValue ?? defaults
+                        .deprecatedValue(for: \.chatGPTModel)
+                )
+            )
+            let azureOpenAI = ChatModel(
+                id: chatModelAzureOpenAIId,
+                name: "Azure OpenAI",
+                format: .azureOpenAI,
+                info: .init(
+                    apiKeyName: azureOpenAIAPIKeyName,
+                    baseURL: defaults.deprecatedValue(for: \.azureOpenAIBaseURL),
+                    maxTokens: defaults.deprecatedValue(for: \.chatGPTMaxToken),
+                    modelName: defaults
+                        .deprecatedValue(for: \.azureChatGPTDeployment)
+                )
+            )
+            
+            return [openAI, azureOpenAI]
+        }(), for: \.chatModels)
+        
+        defaults.set({
+            if defaults.deprecatedValue(for: \.chatFeatureProvider) == .azureOpenAI {
+                return chatModelAzureOpenAIId
+            }
+            return chatModelOpenAIId
+        }(), for: \.defaultChatFeatureChatModelId)
+        
+        defaults.set({
+            let openAIModel = OpenAIEmbeddingModel(
+                rawValue: defaults.deprecatedValue(for: \.embeddingModel)
+            )
+            
+            let openAI = EmbeddingModel(
+                id: embeddingModelOpenAIId,
+                name: "OpenAI",
+                format: .openAI,
+                info: .init(
+                    apiKeyName: openAIAPIKeyName,
+                    baseURL: defaults.deprecatedValue(for: \.openAIBaseURL),
+                    maxTokens: openAIModel?.maxToken ?? 8191,
+                    modelName: openAIModel?.rawValue ?? defaults.deprecatedValue(for: \.embeddingModel)
+                )
+            )
+            
+            let azureOpenAI = EmbeddingModel(
+                id: embeddingModelAzureOpenAIId,
+                name: "Azure OpenAI",
+                format: .azureOpenAI,
+                info: .init(
+                    apiKeyName: azureOpenAIAPIKeyName,
+                    baseURL: defaults.deprecatedValue(for: \.azureOpenAIBaseURL),
+                    maxTokens: 8191,
+                    modelName: defaults
+                        .deprecatedValue(for: \.azureEmbeddingDeployment)
+                )
+            )
+            
+            return [openAI, azureOpenAI]
+        }(), for: \.embeddingModels)
+        
+        defaults.set({
+            if defaults.deprecatedValue(for: \.embeddingFeatureProvider) == .azureOpenAI {
+                return embeddingModelAzureOpenAIId
+            }
+            return embeddingModelOpenAIId
+        }(), for: \.defaultChatFeatureEmbeddingModelId)
+        
+        defaults.set(true, forKey: finishedMigrationKey)
+    } catch {
+        print(error.localizedDescription)
+        throw error
+    }
 }
 
