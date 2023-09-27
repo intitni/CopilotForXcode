edc8c29301be248f558dbe32b6bc0d00b50b1c55
Shx Guo <int123c@gmail.com>
2023-09-27 16:02:25 +0800

    Check if to include pro package with a configuration file

--- Core/Sources/ChatService/AllContextCollector.swift
+++ Core/Sources/ChatService/AllContextCollector.swift
@@ -2,12 +2,19 @@ import ActiveDocumentChatContextCollector
 import ChatContextCollector
 import SystemInfoChatContextCollector
 import WebChatContextCollector
+#if canImport(ProChatContextCollectors)
 import ProChatContextCollectors
-
 let allContextCollectors: [any ChatContextCollector] = [
     SystemInfoChatContextCollector(),
     ActiveDocumentChatContextCollector(),
     WebChatContextCollector(),
     ProChatContextCollectors(),
 ]
+#else
+let allContextCollectors: [any ChatContextCollector] = [
+    SystemInfoChatContextCollector(),
+    ActiveDocumentChatContextCollector(),
+    WebChatContextCollector(),
+]
+#endif
 
