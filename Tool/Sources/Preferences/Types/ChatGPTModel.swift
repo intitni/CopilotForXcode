4a3959168f83580a748769965e84cbfcb1f0ae8d
Shx Guo <int123c@gmail.com>
2023-11-09 00:38:00 +0800

    Add new OpenAI model

--- Tool/Sources/Preferences/Types/ChatGPTModel.swift
+++ Tool/Sources/Preferences/Types/ChatGPTModel.swift
@@ -7,8 +7,11 @@ public enum ChatGPTModel: String {
     case gpt432k = "gpt-4-32k"
     case gpt40314 = "gpt-4-0314"
     case gpt40613 = "gpt-4-0613"
+    case gpt41106Preview = "gpt-4-1106-preview"
+    case gpt4VisionPreview = "gpt-4-vision-preview"
     case gpt35Turbo0301 = "gpt-3.5-turbo-0301"
     case gpt35Turbo0613 = "gpt-3.5-turbo-0613"
+    case gpt35Turbo1106 = "gpt-3.5-turbo-1106"
     case gpt35Turbo16k0613 = "gpt-3.5-turbo-16k-0613"
     case gpt432k0314 = "gpt-4-32k-0314"
     case gpt432k0613 = "gpt-4-32k-0613"
@@ -31,14 +34,29 @@ public extension ChatGPTModel {
             return 4096
         case .gpt35Turbo0613:
             return 4096
+        case .gpt35Turbo1106:
+            return 16385
         case .gpt35Turbo16k:
-            return 16384
+            return 16385
         case .gpt35Turbo16k0613:
-            return 16384
+            return 16385
         case .gpt40613:
             return 8192
         case .gpt432k0613:
             return 32768
+        case .gpt41106Preview:
+            return 128000
+        case .gpt4VisionPreview:
+            return 128000
+        }
+    }
+    
+    var supportsImages: Bool {
+        switch self {
+        case .gpt4VisionPreview:
+            return true
+        default:
+            return false
         }
     }
 }
