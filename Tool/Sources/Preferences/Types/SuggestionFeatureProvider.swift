8593273f9495927c73e9ae4930aa9b9d7590e00a
Shx Guo <int123c@gmail.com>
2023-12-25 16:43:55 +0800

    Fix a crash

--- Tool/Sources/Preferences/Types/SuggestionFeatureProvider.swift
+++ Tool/Sources/Preferences/Types/SuggestionFeatureProvider.swift
@@ -5,20 +5,35 @@ public enum BuiltInSuggestionFeatureProvider: Int, CaseIterable, Codable {
     case codeium
 }
 
-public enum SuggestionFeatureProvider: Codable, RawRepresentable, Hashable {
+public enum SuggestionFeatureProvider: RawRepresentable, Hashable {
     case builtIn(BuiltInSuggestionFeatureProvider)
     case `extension`(name: String, bundleIdentifier: String)
 
+    enum Storage: Codable {
+        case builtIn(BuiltInSuggestionFeatureProvider)
+        case `extension`(name: String, bundleIdentifier: String)
+    }
+
     public init?(rawValue: String) {
         guard let data = rawValue.data(using: .utf8),
-              let value = try? JSONDecoder().decode(Self.self, from: data)
+              let value = try? JSONDecoder().decode(Storage.self, from: data)
         else { return nil }
 
-        self = value
+        switch value {
+        case let .builtIn(provider):
+            self = .builtIn(provider)
+        case let .extension(name, bundleIdentifier):
+            self = .extension(name: name, bundleIdentifier: bundleIdentifier)
+        }
     }
 
     public var rawValue: String {
-        if let data = try? JSONEncoder().encode(self) {
+        let storage: Storage = switch self {
+        case let .builtIn(provider): .builtIn(provider)
+        case let .extension(name, bundleIdentifier):
+            .extension(name: name, bundleIdentifier: bundleIdentifier)
+        }
+        if let data = try? JSONEncoder().encode(storage) {
             return String(data: data, encoding: .utf8) ?? ""
         }
         return ""
