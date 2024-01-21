670619948a06d978fd0c948a92866e25bf027e6b
Shx Guo <int123c@gmail.com>
2024-01-22 01:00:39 +0800

    Add tests

--- Tool/Tests/OpenAIServiceTests/AutoManagedChatGPTMemoryRetrievedContentTests.swift
+++ Tool/Tests/OpenAIServiceTests/AutoManagedChatGPTMemoryRetrievedContentTests.swift
@@ -159,6 +159,70 @@ class AutoManagedChatGPTMemoryRetrievedContentTests: XCTestCase {
         D
         """)
     }
+    
+    func test_retrieved_content_when_the_context_window_can_take_only_one_document() async {
+        let strategy = Strategy()
+
+        let memory = AutoManagedChatGPTMemory(
+            systemPrompt: "",
+            configuration: UserPreferenceChatGPTConfiguration(),
+            functionProvider: EmptyFunctionProvider()
+        )
+
+        await memory.mutateRetrievedContent([
+            ref("A"), ref("B"), ref("C"), ref("D"), ref("E"),
+        ])
+
+        let fullContent = """
+        Here are the information you know about the system and the project, \
+        separated by \(separator)
+
+        \(separator)[DOCUMENT 0]
+
+        A
+        """
+
+        let maxTokenCount = await strategy.countToken(.init(role: .user, content: fullContent))
+
+        let result = await memory.generateRetrievedContentMessage(
+            maxTokenCount: maxTokenCount + 1,
+            strategy: strategy
+        )
+
+        XCTAssertEqual(result.references.count, 1)
+        XCTAssertEqual(result.retrievedContent.role, .user)
+        XCTAssertEqual(result.retrievedContent.content, """
+        Here are the information you know about the system and the project, \
+        separated by \(separator)
+
+        \(separator)[DOCUMENT 0]
+
+        A
+        """)
+    }
+    
+    func test_retrieved_content_when_the_context_window_empty() async {
+        let strategy = Strategy()
+
+        let memory = AutoManagedChatGPTMemory(
+            systemPrompt: "",
+            configuration: UserPreferenceChatGPTConfiguration(),
+            functionProvider: EmptyFunctionProvider()
+        )
+
+        await memory.mutateRetrievedContent([
+            ref("A"), ref("B"), ref("C"), ref("D"), ref("E"),
+        ])
+
+        let result = await memory.generateRetrievedContentMessage(
+            maxTokenCount: 0,
+            strategy: strategy
+        )
+
+        XCTAssertEqual(result.references.count, 0)
+        XCTAssertEqual(result.retrievedContent.role, .user)
+        XCTAssertEqual(result.retrievedContent.content, "")
+    }
 }
 
 private struct EmptyFunctionProvider: ChatGPTFunctionProvider {
