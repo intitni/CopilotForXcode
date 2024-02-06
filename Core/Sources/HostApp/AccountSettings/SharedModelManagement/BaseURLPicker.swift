519f0aec472c9bc5652e46e8ba5513e26608758e
Ge Will <531sunlight@gmail.com>
2024-02-07 01:51:46 +0800

    Modify Full URL UI to segment control

--- Core/Sources/HostApp/AccountSettings/SharedModelManagement/BaseURLPicker.swift
+++ Core/Sources/HostApp/AccountSettings/SharedModelManagement/BaseURLPicker.swift
@@ -9,40 +9,48 @@ struct BaseURLPicker: View {
     var body: some View {
         WithViewStore(store) { viewStore in
             Group {
-                TextField("Base URL", text: viewStore.$baseURL, prompt: prompt)
-                    .overlay(alignment: .trailing) {
-                        Picker(
-                            "",
-                            selection: viewStore.$baseURL,
-                            content: {
-                                if !viewStore.state.availableBaseURLs
-                                    .contains(viewStore.state.baseURL),
-                                   !viewStore.state.baseURL.isEmpty
-                                {
-                                    Text("Custom Value").tag(viewStore.state.baseURL)
-                                }
-                                
-                                Text("Empty (Default Value)").tag("")
-                                
-                                ForEach(viewStore.state.availableBaseURLs, id: \.self) { baseURL in
-                                    Text(baseURL).tag(baseURL)
-                                }
-                            }
-                        )
-                        .frame(width: 20)
-                    }
                 if showIsFullURL {
-                    Toggle(
-                        "Is Base URL with Full Path",
-                        isOn: viewStore.$isFullURL
+                    Picker(
+                        selection: viewStore.$isFullURL,
+                        content: {
+                            Text("Base URL").tag(false)
+                            Text("Full URL").tag(true)
+                        },
+                        label: { Text("URL") }
                     )
-                    
-                    Text(
-                        "Add compatibility API's distinct endpoint structure. For example Perplexity.ai API's URL is https://api.perplexity.ai/chat/completions"
+                    .pickerStyle(.segmented)
+                }
+                HStack {
+                    TextField(
+                        showIsFullURL ? "" : "Base URL",
+                        text: viewStore.$baseURL,
+                        prompt: prompt
                     )
-                    .foregroundColor(.secondary)
-                    .font(.callout)
-                    .dynamicHeightTextInFormWorkaround()
+                    if viewStore.isFullURL == false {
+                        Text("/v1/chat/completions")
+                    }
+                }
+                .padding(.trailing)
+                .overlay(alignment: .trailing) {
+                    Picker(
+                        "",
+                        selection: viewStore.$baseURL,
+                        content: {
+                            if !viewStore.state.availableBaseURLs
+                                .contains(viewStore.state.baseURL),
+                               !viewStore.state.baseURL.isEmpty
+                            {
+                                Text("Custom Value").tag(viewStore.state.baseURL)
+                            }
+                            
+                            Text("Empty (Default Value)").tag("")
+                            
+                            ForEach(viewStore.state.availableBaseURLs, id: \.self) { baseURL in
+                                Text(baseURL).tag(baseURL)
+                            }
+                        }
+                    )
+                    .frame(width: 20)
                 }
             }
             .onAppear {
