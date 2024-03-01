d3c02a12df565642398a71b5daf11ba6e51cc539
Shx Guo <int123c@gmail.com>
2024-03-01 23:38:54 +0800

    Convert API types to OpenAIService

--- Tool/Sources/OpenAIService/APIs/OpenAIService.swift
+++ Tool/Sources/OpenAIService/APIs/OpenAIService.swift
@@ -3,7 +3,20 @@ import AsyncAlgorithms
 import Foundation
 import Preferences
 
-struct OpenAICompletionStreamAPI: ChatCompletionsStreamAPI {
+actor OpenAIService: ChatCompletionsStreamAPI, ChatCompletionsAPI {
+    struct CompletionAPIError: Error, Codable, LocalizedError {
+        struct E: Codable {
+            var message: String
+            var type: String
+            var param: String
+            var code: String
+        }
+
+        var error: E
+
+        var errorDescription: String? { error.message }
+    }
+
     var apiKey: String
     var endpoint: URL
     var requestBody: ChatCompletionsRequestBody
@@ -18,11 +31,13 @@ struct OpenAICompletionStreamAPI: ChatCompletionsStreamAPI {
         self.apiKey = apiKey
         self.endpoint = endpoint
         self.requestBody = requestBody
-        self.requestBody.stream = true
         self.model = model
     }
 
-    func callAsFunction() async throws -> AsyncThrowingStream<ChatCompletionsStreamDataChunk, Error> {
+    func callAsFunction() async throws
+        -> AsyncThrowingStream<ChatCompletionsStreamDataChunk, Error>
+    {
+        requestBody.stream = true
         var request = URLRequest(url: endpoint)
         request.httpMethod = "POST"
         let encoder = JSONEncoder()
@@ -80,6 +95,43 @@ struct OpenAICompletionStreamAPI: ChatCompletionsStreamAPI {
         }
 
         return stream
+    }
+
+    func callAsFunction() async throws -> ChatCompletionResponseBody {
+        requestBody.stream = false
+        var request = URLRequest(url: endpoint)
+        request.httpMethod = "POST"
+        let encoder = JSONEncoder()
+        request.httpBody = try encoder.encode(requestBody)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        if !apiKey.isEmpty {
+            switch model.format {
+            case .openAI, .openAICompatible:
+                request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+            case .azureOpenAI:
+                request.setValue(apiKey, forHTTPHeaderField: "api-key")
+            case .googleAI:
+                assertionFailure("Unsupported")
+            }
+        }
+
+        let (result, response) = try await URLSession.shared.data(for: request)
+        guard let response = response as? HTTPURLResponse else {
+            throw ChatGPTServiceError.responseInvalid
+        }
+
+        guard response.statusCode == 200 else {
+            let error = try? JSONDecoder().decode(CompletionAPIError.self, from: result)
+            throw error ?? ChatGPTServiceError
+                .otherError(String(data: result, encoding: .utf8) ?? "Unknown Error")
+        }
+
+        do {
+            return try JSONDecoder().decode(ChatCompletionResponseBody.self, from: result)
+        } catch {
+            dump(error)
+            throw error
+        }
     }
 }
 
