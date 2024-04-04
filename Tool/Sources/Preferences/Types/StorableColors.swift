6447b3ca55e70b443cff3f7c9d097e12171a5a94
Shx Guo <int123c@gmail.com>
2024-04-04 18:26:27 +0800

    Add preference keys for theme

---
+++ Tool/Sources/Preferences/Types/StorableColors.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+public struct StorableColor: Codable {
+    public var red: Double
+    public var green: Double
+    public var blue: Double
+    public var alpha: Double
+
+    public init(red: Double, green: Double, blue: Double, alpha: Double) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+    }
+}
+
+#if canImport(SwiftUI)
+import SwiftUI
+public extension StorableColor {
+    var swiftUIColor: SwiftUI.Color {
+        SwiftUI.Color(.sRGB, red: red, green: green, blue: blue, opacity: alpha)
+    }
+}
+#endif
+
+#if canImport(AppKit)
+import AppKit
+public extension StorableColor {
+    var nsColor: NSColor {
+        NSColor(
+            srgbRed: CGFloat(red),
+            green: CGFloat(green),
+            blue: CGFloat(blue),
+            alpha: CGFloat(alpha)
+        )
+    }
+}
+#endif
+
