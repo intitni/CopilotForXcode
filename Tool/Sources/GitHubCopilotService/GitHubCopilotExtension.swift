cb901f9c58726229060efedfbb495101af8203ec
Shx Guo <int123c@gmail.com>
2024-05-20 15:36:12 +0800

    Convert GitHub Copilot to a builtin extension

---
+++ Tool/Sources/GitHubCopilotService/GitHubCopilotExtension.swift
@@ -0,0 +1,123 @@
+import CopilotForXcodeKit
+import Foundation
+import Workspace
+import Logger
+import BuiltinExtension
+
+public final class GitHubCopilotExtension: BuiltinExtension {
+    public var suggestionService: SuggestionServiceType? { _suggestionService }
+    public var chatService: ChatServiceType? { nil }
+    public var promptToCodeService: PromptToCodeServiceType? { nil }
+    let workspacePool: WorkspacePool
+
+    let serviceLocator: ServiceLocator
+    let _suggestionService: GitHubCopilotSuggestionService
+
+    init(workspacePool: WorkspacePool) {
+        self.workspacePool = workspacePool
+        serviceLocator = .init(workspacePool: workspacePool)
+        _suggestionService = .init(serviceLocator: serviceLocator)
+    }
+
+    public func workspaceDidOpen(_: WorkspaceInfo) {}
+
+    public func workspaceDidClose(_: WorkspaceInfo) {}
+
+    public func workspace(_ workspace: WorkspaceInfo, didOpenDocumentAt documentURL: URL) {
+        // check if file size is larger than 15MB, if so, return immediately
+        if let attrs = try? FileManager.default
+            .attributesOfItem(atPath: documentURL.path),
+            let fileSize = attrs[FileAttributeKey.size] as? UInt64,
+            fileSize > 15 * 1024 * 1024
+        { return }
+
+        Task {
+            do {
+                let content = try String(contentsOf: documentURL, encoding: .utf8)
+                guard let service = await serviceLocator.getService(from: workspace) else { return }
+                try await service.notifyOpenTextDocument(fileURL: documentURL, content: content)
+            } catch {
+                Logger.gitHubCopilot.error(error.localizedDescription)
+            }
+        }
+    }
+
+    public func workspace(_ workspace: WorkspaceInfo, didSaveDocumentAt documentURL: URL) {
+        Task {
+            do {
+                guard let service = await serviceLocator.getService(from: workspace) else { return }
+                try await service.notifySaveTextDocument(fileURL: documentURL)
+            } catch {
+                Logger.gitHubCopilot.error(error.localizedDescription)
+            }
+        }
+    }
+
+    public func workspace(_ workspace: WorkspaceInfo, didCloseDocumentAt documentURL: URL) {
+        Task {
+            do {
+                guard let service = await serviceLocator.getService(from: workspace) else { return }
+                try await service.notifyCloseTextDocument(fileURL: documentURL)
+            } catch {
+                Logger.gitHubCopilot.error(error.localizedDescription)
+            }
+        }
+    }
+
+    public func workspace(
+        _ workspace: WorkspaceInfo,
+        didUpdateDocumentAt documentURL: URL,
+        content: String
+    ) {
+        // check if file size is larger than 15MB, if so, return immediately
+        if let attrs = try? FileManager.default
+            .attributesOfItem(atPath: documentURL.path),
+            let fileSize = attrs[FileAttributeKey.size] as? UInt64,
+            fileSize > 15 * 1024 * 1024
+        { return }
+        
+        Task {
+            do {
+                let content = try String(contentsOf: documentURL, encoding: .utf8)
+                guard let service = await serviceLocator.getService(from: workspace) else { return }
+                try await service.notifyOpenTextDocument(fileURL: documentURL, content: content)
+            } catch {
+                Logger.gitHubCopilot.error(error.localizedDescription)
+            }
+        }
+    }
+
+    public func appConfigurationDidChange(_ configuration: AppConfiguration) {
+        if !configuration.chatServiceInUse && !configuration.suggestionServiceInUse {
+            for workspace in workspacePool.workspaces.values {
+                guard let plugin = workspace.plugin(for: GitHubCopilotWorkspacePlugin.self)
+                else { continue }
+                plugin.terminate()
+            }
+        }
+    }
+    
+    public func terminate() {
+        for workspace in workspacePool.workspaces.values {
+            guard let plugin = workspace.plugin(for: GitHubCopilotWorkspacePlugin.self)
+            else { continue }
+            plugin.terminate()
+        }
+    }
+}
+
+final class ServiceLocator {
+    let workspacePool: WorkspacePool
+
+    init(workspacePool: WorkspacePool) {
+        self.workspacePool = workspacePool
+    }
+
+    func getService(from workspace: WorkspaceInfo) async -> GitHubCopilotService? {
+        guard let workspace = workspacePool.workspaces[workspace.workspaceURL],
+              let plugin = workspace.plugin(for: GitHubCopilotWorkspacePlugin.self)
+        else { return nil }
+        return plugin.gitHubCopilotService
+    }
+}
+
---
+++ Tool/Sources/GitHubCopilotService/GitHubCopilotWorkspacePlugin.swift
@@ -0,0 +1,49 @@
+import Foundation
+import Workspace
+
+public final class GitHubCopilotWorkspacePlugin: WorkspacePlugin {
+    var _gitHubCopilotService: GitHubCopilotService?
+    var gitHubCopilotService: GitHubCopilotService? {
+        if let service = _gitHubCopilotService { return service }
+        do {
+            return try createGitHubCopilotService()
+        } catch {
+            return nil
+        }
+    }
+
+    deinit {
+        if let gitHubCopilotService {
+            Task { await gitHubCopilotService.terminate() }
+        }
+    }
+
+    func createGitHubCopilotService() throws -> GitHubCopilotService {
+        let newService = try GitHubCopilotService(projectRootURL: projectRootURL)
+        _gitHubCopilotService = newService
+        Task {
+            try await Task.sleep(nanoseconds: 1_000_000_000)
+            finishLaunchingService()
+        }
+        return newService
+    }
+
+    func finishLaunchingService() {
+        guard let workspace, let _gitHubCopilotService else { return }
+        Task {
+            for (_, filespace) in workspace.filespaces {
+                let documentURL = filespace.fileURL
+                guard let content = try? String(contentsOf: documentURL) else { continue }
+                try? await _gitHubCopilotService.notifyOpenTextDocument(
+                    fileURL: documentURL,
+                    content: content
+                )
+            }
+        }
+    }
+
+    func terminate() {
+        _gitHubCopilotService = nil
+    }
+}
+
