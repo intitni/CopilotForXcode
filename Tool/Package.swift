d456d583b3749c46e8706bdd49bf90078964a7f8
Shx Guo <int123c@gmail.com>
2024-06-22 23:58:47 +0800

    Update CopilotForXcodeKit

--- Tool/Package.swift
+++ Tool/Package.swift
@@ -71,7 +71,7 @@ let package = Package(
             url: "https://github.com/intitni/generative-ai-swift",
             branch: "support-setting-base-url"
         ),
-        .package(url: "https://github.com/intitni/CopilotForXcodeKit", branch: "develop"),
+        .package(url: "https://github.com/intitni/CopilotForXcodeKit", from: "0.7.1"),
 
         // TreeSitter
         .package(url: "https://github.com/intitni/SwiftTreeSitter.git", branch: "main"),
