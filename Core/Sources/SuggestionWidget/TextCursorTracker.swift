d7fad1c8228e05e969bf8f88601f59290cd6b6b9
Shx Guo <int123c@gmail.com>
2024-07-14 21:54:56 +0800

    Update TextCursorTracker

--- Core/Sources/SuggestionWidget/TextCursorTracker.swift
+++ Core/Sources/SuggestionWidget/TextCursorTracker.swift
@@ -4,12 +4,29 @@ import Perception
 import SuggestionBasic
 import XcodeInspector
 
+/// A passive tracker that observe the changes of the source editor content.
 @Perceptible
 final class TextCursorTracker {
     @MainActor
-    var cursorPosition: CursorPosition = .zero
+    var cursorPosition: CursorPosition { content.cursorPosition }
     @MainActor
-    var currentLine: String = ""
+    var currentLine: String {
+        if content.cursorPosition.line >= 0, content.cursorPosition.line < content.lines.count {
+            content.lines[content.cursorPosition.line]
+        } else {
+            ""
+        }
+    }
+
+    @MainActor
+    var content: SourceEditor.Content = .init(
+        content: "",
+        lines: [],
+        selections: [],
+        cursorPosition: .zero,
+        cursorOffset: 0,
+        lineAnnotations: []
+    )
 
     @PerceptionIgnored var editorObservationTask: Set<AnyCancellable> = []
     @PerceptionIgnored var eventObservationTask: Task<Void, Never>?
@@ -38,14 +55,7 @@ final class TextCursorTracker {
         eventObservationTask?.cancel()
         let content = editor.getLatestEvaluatedContent()
         Task { @MainActor in
-            self.cursorPosition = content.cursorPosition
-            self.currentLine = if content.cursorPosition.line >= 0,
-                                  content.cursorPosition.line < content.lines.count
-            {
-                content.lines[content.cursorPosition.line]
-            } else {
-                ""
-            }
+            self.content = content
         }
         eventObservationTask = Task { [weak self] in
             for await event in await editor.axNotifications.notifications() {
@@ -53,7 +63,7 @@ final class TextCursorTracker {
                 guard event.kind == .evaluatedContentChanged else { continue }
                 let content = editor.getLatestEvaluatedContent()
                 Task { @MainActor in
-                    self.cursorPosition = content.cursorPosition
+                    self.content = content
                 }
             }
         }
