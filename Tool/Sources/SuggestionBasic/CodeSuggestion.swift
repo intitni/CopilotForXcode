cab09478fb8519a7afccdfd84ef27d3b54a70c5f
Shx Guo <int123c@gmail.com>
2024-07-14 16:43:03 +0800

    Add description to CodeSuggestion

--- Tool/Sources/SuggestionBasic/CodeSuggestion.swift
+++ Tool/Sources/SuggestionBasic/CodeSuggestion.swift
@@ -1,12 +1,28 @@
-import Foundation
 import CodableWrappers
+import Foundation
 
 public struct CodeSuggestion: Codable, Equatable {
+    public struct Description: Codable, Equatable {
+        public enum Kind: Codable, Equatable {
+            case warning
+            case action
+        }
+        
+        public var kind: Kind
+        public var content: String
+        
+        public init(kind: Kind, content: String) {
+            self.kind = kind
+            self.content = content
+        }
+    }
+    
     public init(
         id: String,
         text: String,
         position: CursorPosition,
         range: CursorRange,
+        descriptions: [Description],
         middlewareComments: [String] = [],
         metadata: [String: String] = [:]
     ) {
@@ -14,15 +30,18 @@ public struct CodeSuggestion: Codable, Equatable {
         self.position = position
         self.id = id
         self.range = range
+        self.descriptions = descriptions
         self.middlewareComments = middlewareComments
         self.metadata = metadata
     }
 
     public static func == (lhs: CodeSuggestion, rhs: CodeSuggestion) -> Bool {
-        return lhs.text == rhs.text 
-        && lhs.position == rhs.position
-        && lhs.id == rhs.id
-        && lhs.range == rhs.range
+        return lhs.text == rhs.text
+            && lhs.position == rhs.position
+            && lhs.id == rhs.id
+            && lhs.range == rhs.range
+            && lhs.descriptions == rhs.descriptions
+            && lhs.middlewareComments == rhs.middlewareComments
     }
 
     /// The new code to be inserted and the original code on the first line.
@@ -33,6 +52,8 @@ public struct CodeSuggestion: Codable, Equatable {
     public var id: String
     /// The range of the original code that should be replaced.
     public var range: CursorRange
+    /// Descriptions about this code suggestion
+    @FallbackDecoding<EmptyArray> public var descriptions: [Description]
     /// A place to store comments inserted by middleware for debugging use.
     @FallbackDecoding<EmptyArray> public var middlewareComments: [String]
     /// A place to store extra data.
