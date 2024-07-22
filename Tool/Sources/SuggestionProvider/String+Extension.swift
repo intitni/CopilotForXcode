67a83ce284e3b6e95637b1a705c4e6ec95f81372
Shx Guo <int123c@gmail.com>
2024-07-22 15:12:31 +0800

    Add extensions to String

--- Tool/Sources/SuggestionProvider/String+Extension.swift
+++ Tool/Sources/SuggestionProvider/String+Extension.swift
@@ -8,4 +8,26 @@ public extension String {
         }
         return String(text)
     }
+    
+    func removedTrailingCharacters(in set: CharacterSet) -> String {
+        var text = self[...]
+        while let last = text.last, set.containsUnicodeScalars(of: last) {
+            text = text.dropLast(1)
+        }
+        return String(text)
+    }
+    
+    func removeLeadingCharacters(in set: CharacterSet) -> String {
+        var text = self[...]
+        while let first = text.first, set.containsUnicodeScalars(of: first) {
+            text = text.dropFirst()
+        }
+        return String(text)
+    }
+}
+
+extension CharacterSet {
+    func containsUnicodeScalars(of character: Character) -> Bool {
+        return character.unicodeScalars.allSatisfy(contains(_:))
+    }
 }
