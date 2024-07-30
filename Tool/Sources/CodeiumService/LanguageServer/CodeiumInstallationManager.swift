71bb266d3079b88ebdd01904218244b7738a5786
Shx Guo <int123c@gmail.com>
2024-07-30 14:50:17 +0800

    Merge pull request #558 from RoshanNagaram-eng/roshan/updatelangserver
    updated lang server version to 1.8.83

--- Tool/Sources/CodeiumService/LanguageServer/CodeiumInstallationManager.swift
+++ Tool/Sources/CodeiumService/LanguageServer/CodeiumInstallationManager.swift
@@ -3,7 +3,7 @@ import Terminal
 
 public struct CodeiumInstallationManager {
     private static var isInstalling = false
-    static let latestSupportedVersion = "1.8.8"
+    static let latestSupportedVersion = "1.8.83"
 
     public init() {}
 
