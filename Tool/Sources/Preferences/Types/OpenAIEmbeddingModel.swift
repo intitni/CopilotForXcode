802fa304cd48d419b8974698fcb6e865da091f0f
Shx Guo <int123c@gmail.com>
2024-10-07 13:08:49 +0800

    Add OpenAI embedding models

--- Tool/Sources/Preferences/Types/OpenAIEmbeddingModel.swift
+++ Tool/Sources/Preferences/Types/OpenAIEmbeddingModel.swift
@@ -1,4 +1,6 @@
 public enum OpenAIEmbeddingModel: String, CaseIterable {
+    case textEmbedding3Small = "text-embedding-3-small"
+    case textEmbedding3Large = "text-embedding-3-large"
     case textEmbeddingAda002 = "text-embedding-ada-002"
 }
 
@@ -6,6 +8,10 @@ public extension OpenAIEmbeddingModel {
     var maxToken: Int {
         switch self {
         case .textEmbeddingAda002:
+            return 8191
+        case .textEmbedding3Small:
+            return 8191
+        case .textEmbedding3Large:
             return 8191
         }
     }
