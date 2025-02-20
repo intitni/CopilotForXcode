6a90966afd3b71791bb8cf2cd80aedc6d791279a
Todor Pitekov <todor.pitekov@pcloud.com>
2025-02-20 09:26:28 +0200

    optionally add api key to header

--- Tool/Sources/OpenAIService/APIs/OlamaChatCompletionsService.swift
+++ Tool/Sources/OpenAIService/APIs/OlamaChatCompletionsService.swift
@@ -59,7 +59,11 @@ extension OllamaChatCompletionsService: ChatCompletionsAPI {
         let encoder = JSONEncoder()
         request.httpBody = try encoder.encode(requestBody)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-		request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+		
+		if !apiKey.isEmpty {
+			request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+		}
+		
         let (result, response) = try await URLSession.shared.data(for: request)
 
         guard let response = response as? HTTPURLResponse else {
@@ -136,7 +140,11 @@ extension OllamaChatCompletionsService: ChatCompletionsStreamAPI {
         let encoder = JSONEncoder()
         request.httpBody = try encoder.encode(requestBody)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-		request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+		
+		if !apiKey.isEmpty {
+			request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+		}
+		
         let (result, response) = try await URLSession.shared.bytes(for: request)
 
         guard let response = response as? HTTPURLResponse else {
