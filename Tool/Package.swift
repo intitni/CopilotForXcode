059a2da27920bac3d31922b62394817db9393d5f
Shx Guo <int123c@gmail.com>
2025-03-10 01:40:58 +0800

    Support custom body

--- Tool/Package.swift
+++ Tool/Package.swift
@@ -460,6 +460,7 @@ let package = Package(
                 "BuiltinExtension",
                 "ChatBasic",
                 "GitHubCopilotService",
+                "JoinJSON",
                 .product(name: "JSONRPC", package: "JSONRPC"),
                 .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
                 .product(name: "GoogleGenerativeAI", package: "generative-ai-swift"),
