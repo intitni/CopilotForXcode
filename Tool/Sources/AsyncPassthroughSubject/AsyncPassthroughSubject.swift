b540fc3c80a2dacd784621943340c8aeef971243
Shx Guo <int123c@gmail.com>
2025-07-22 17:24:12 +0800

    Make AsyncPassthroughSubject tasks removable

--- Tool/Sources/AsyncPassthroughSubject/AsyncPassthroughSubject.swift
+++ Tool/Sources/AsyncPassthroughSubject/AsyncPassthroughSubject.swift
@@ -2,51 +2,59 @@ import AppKit
 import Foundation
 
 public actor AsyncPassthroughSubject<Element> {
-    var tasks: [AsyncStream<Element>.Continuation] = []
+    var tasks: [UUID: AsyncStream<Element>.Continuation] = [:]
 
     deinit {
-        tasks.forEach { $0.finish() }
+        tasks.values.forEach { $0.finish() }
     }
-    
+
     public init() {}
 
     public func notifications() -> AsyncStream<Element> {
         AsyncStream { [weak self] continuation in
+            let id = UUID()
             let task = Task { [weak self] in
-                await self?.storeContinuation(continuation)
+                await self?.storeContinuation(continuation, for: id)
             }
-            
-            continuation.onTermination = { termination in
+
+            continuation.onTermination = { [weak self] _ in
                 task.cancel()
+                Task {
+                    await self?.removeContinuation(for: id)
+                }
             }
         }
     }
 
-    nonisolated
-    public func send(_ element: Element) {
+    public nonisolated
+    func send(_ element: Element) {
         Task { await _send(element) }
     }
-    
+
     func _send(_ element: Element) {
         let tasks = tasks
-        for task in tasks {
+        for task in tasks.values {
             task.yield(element)
         }
     }
-    
-    func storeContinuation(_ continuation: AsyncStream<Element>.Continuation) {
-        tasks.append(continuation)
+
+    func storeContinuation(_ continuation: AsyncStream<Element>.Continuation, for id: UUID) {
+        tasks[id] = continuation
     }
-    
-    nonisolated
-    public func finish() {
+
+    func removeContinuation(for id: UUID) {
+        tasks.removeValue(forKey: id)
+    }
+
+    public nonisolated
+    func finish() {
         Task { await _finish() }
     }
-    
+
     func _finish() {
         let tasks = self.tasks
-        self.tasks = []
-        for task in tasks {
+        self.tasks = [:]
+        for task in tasks.values {
             task.finish()
         }
     }
